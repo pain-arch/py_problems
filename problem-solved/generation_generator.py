9d2e7faae9c0a4591193befb4a898f29e78b8151
pain-arch <himulol123@gmail.com>
2024-03-31 19:00:04 +0600

    1st

---
+++ problem-solved/generation_generator.py
@@ -0,0 +1,11 @@
+birthYear = int(input("what is your birth year?"))
+if birthYear >= 1925 and birthYear <= 1946:
+  print("You are a traditioanlist")
+elif birthYear >= 1947 and birthYear <= 1964:
+  print("You are a Baby Boomer")
+elif birthYear >= 1965 and birthYear <= 1981:
+  print("You are a Gen X")
+elif birthYear >= 1982 and birthYear <= 1995:
+  print("You are a Millenials")
+elif birthYear >= 1996 and birthYear <= 2015:
+  print("You are a Gen z")
