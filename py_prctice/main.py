9ebf078328d4971dcff01dfc442042b7ce8ec58f
pain-arch <himulol123@gmail.com>
2024-04-23 17:10:56 +0600

    Add Person class and greet method in main.py

--- py_prctice/main.py
+++ py_prctice/main.py
@@ -98,3 +98,17 @@ def fdiv(a, b):
 add(10, 5)
 sub(10, 5)
 mul(10, 5)
+
+# Classes
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old")
+
+person1 = Person("John", 25)
+person1.greet()
+
