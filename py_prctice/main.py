4d0c079ad06e4a43b3e8ecb7e30bb43fdc637264
pain-arch <himulol123@gmail.com>
2024-04-23 17:13:31 +0600

    Add exception handling for zero division in main.py

--- py_prctice/main.py
+++ py_prctice/main.py
@@ -135,3 +135,15 @@ file = open("file.txt", "r")
 content = file.read()
 print(content)
 file.close()
+
+
+# Exception Handling
+
+try:
+    a = 10 / 0
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+finally:
+    print("This will always run")
+
+# 5. Advanced
